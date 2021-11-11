e0770a63abc6263f3418e175cf05cd8808682ae4
ndx2527 <1378362527@qq.com>
2021-11-11 09:25:28 +0800

    11.11页面标签

---
+++ 标签栏/使用uview.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="content">
+		<u-tabs name="cate_name" count="cate_count" :list="list" :is-scroll="true" :current="activeIndex"
+			@change="change">
+		</u-tabs>
+		<view class="swiper">
+			<swiper class="swiper-container" :current="activeIndex" @change="changeCurrentIndex">
+				<swiper-item v-for="(item,index) in list" :key="index">
+					<!-- 滚动区域 -->
+					<scroll-view class="swiper-container-list" scroll-y="true">
+						{{item.name}}
+					</scroll-view>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [{
+					name: '待收货01',
+				}, {
+					name: '待付款02',
+				}, {
+					name: '待评价03',
+				}, {
+					name: '待评价04',
+				}, {
+					name: '待评价05',
+				}, {
+					name: '待评价06',
+				}],
+				activeIndex: 0
+			}
+		},
+		methods: {
+			change(index) {
+				this.activeIndex = index;
+			},
+			changeCurrentIndex(e) {
+				const {
+					current
+				} = e.detail;
+				this.activeIndex = current;
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ 标签栏/未使用u-tab.vue
@@ -0,0 +1,70 @@
+<template>
+	<view>
+		<!-- 标签 -->
+		<!-- :scroll-left='activeIndex*80'设置横向跟着移动 -->
+		<scroll-view class="tab" scroll-x="true">
+			<view class="tab-box">
+				<view class="tab-box-item" v-for="(item,index) in list" :key="index" @click="navClickFn(index)"
+					:class="{ active: activeIndex === index }">
+					{{item}}
+				</view>
+			</view>
+		</scroll-view>
+		<!-- 轮播页面 -->
+		<view class="swiper">
+			<swiper class="swiper-container" :current="activeIndex" @change="changeCurrentIndex">
+				<swiper-item v-for="(item,index) in list" :key="index">
+					<!-- 滚动区域 -->
+					<scroll-view class="swiper-container-list" scroll-y="true">
+						{{item}}
+					</scroll-view>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: ['前端开发01', '前端开发02', '前端开发03', '前端开发04', '前端开发05', '前端开发06'],
+				activeIndex: 0,
+			}
+		},
+		methods: {
+			navClickFn(index) {
+				this.activeIndex = index;
+			},
+			changeCurrentIndex(e) {
+				const {
+					current
+				} = e.detail;
+				this.activeIndex = current;
+			},
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.tab {
+		height: 40px;
+		width: 100%;
+
+		&-box {
+			display: flex;
+
+			&-item {
+				line-height: 40px;
+				padding: 0 10px;
+				flex-shrink: 0;
+
+				&.active {
+					color: #FF0000;
+				}
+			}
+		}
+
+	}
+</style>
