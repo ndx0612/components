1b51df447b372b5912b01eebf3e90d4fa8805c04
ndx2527 <1378362527@qq.com>
2022-05-07 16:33:39 +0800

    feat：滚动条和动态宽度变化

---
+++ css控制宽度变化/index.js
@@ -0,0 +1,34 @@
+function changeUp(dom, time, width_px) {
+  var _this = this;
+  var width = dom.offsetWidth;
+  console.log(width, width_px);
+  if (width < width_px) {
+    dom.style.width = width + 14 + "px";
+    console.log(width, width_px);
+    setTimeout(() => {
+      _this.changeUp(dom, time, width_px);
+    }, time);
+  }
+};
+function changeDown(dom, time, width_px) {
+  var _this = this;
+  var width = dom.offsetWidth;
+  console.log(width, width_px);
+  if (width > width_px) {
+    dom.style.width = width - 14 + "px";
+    console.log(width, width_px);
+    setTimeout(() => {
+      _this.changeDown(dom, time, width_px);
+    }, time);
+  }
+};
+// 点击展开收齐
+function packMenu() {
+  if (data.packUp) {
+    // methods.changeDown(tbody, 10, 180);
+    data.packUp = false;
+  } else {
+    // methods.changeUp(tbody, 10, 320);
+    data.packUp = true;
+  }
+}
