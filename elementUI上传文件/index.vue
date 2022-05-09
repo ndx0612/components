c94ebb9ec5eb667946522a85ac8cd714b890f386
ndx2527 <1378362527@qq.com>
2022-05-09 15:31:22 +0800

    fix：默认文件列表

--- elementUI上传文件/index.vue
+++ elementUI上传文件/index.vue
@@ -29,7 +29,16 @@ import { reactive, getCurrentInstance } from "vue";
 const { proxy } = getCurrentInstance();
 
 const data = reactive({
-  fileList: [], // 上传文件列表
+  fileList: [
+    {
+      name: "food.jpeg",
+      url: "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
+    },
+    {
+      name: "food2.jpeg",
+      url: "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
+    },
+  ], // 上传文件列表
 });
 
 const methods = reactive({
