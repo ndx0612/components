59f333814bc09b1a16d778294d85e6a4691c350c
ndx2527 <1378362527@qq.com>
2022-08-10 10:50:44 +0800

    feat：子元素添加样式

---
+++ 字元素添加样式/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="w-full bg-[#FFF] mt-[20px] tool_box">
+      <el-input v-model="aa" class="w-[200px]"></el-input>
+      <el-input v-model="aa" class="w-[200px]"></el-input>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { getCurrentInstance, onMounted, reactive, ref } from "vue";
+const aa = ref("");
+</script>
+
+<style lang='scss' scoped>
+.tool_box {
+  > :deep(*) {
+    margin: 8px;
+  }
+}
+</style>
