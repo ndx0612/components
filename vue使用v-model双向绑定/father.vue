1b9116afbe5bd21496d97c3dbe72578515641864
ndx2527 <1378362527@qq.com>
2022-09-28 13:32:55 +0800

    feat：父子组件通信（v-model）

---
+++ vue使用v-model双向绑定/father.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <processSubmit v-model:modelValue="text1"></processSubmit>
+    <el-button @click="aa">按钮</el-button>
+  </div>
+</template>
+
+<script setup>
+import { getCurrentInstance, onMounted, reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+let router = useRouter();
+const { proxy } = getCurrentInstance();
+const { query } = router.currentRoute.value;
+onMounted(() => {});
+
+const text1 = ref("");
+
+const aa = () => {
+  console.log(text1.value);
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ vue使用v-model双向绑定/son1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-[300px]">
+    <input :value="modelValue" placeholder="Please input" @input="$emit('update:modelValue', $event.target.value)" />
+  </div>
+</template>
+
+<script setup>
+import { getCurrentInstance, onMounted, reactive, ref, watch } from "vue";
+import { useRouter } from "vue-router";
+defineOptions({ name: "process-submit" });
+
+let router = useRouter();
+const { proxy } = getCurrentInstance();
+const { query } = router.currentRoute.value;
+
+const props = defineProps([
+  "modelValue", // 接收父组件使用 v-model 传进来的值，必须用 modelValue 这个名字来接收
+]);
+
+onMounted(() => {});
+</script>
+
+<style lang='scss' scoped>
+</style>
